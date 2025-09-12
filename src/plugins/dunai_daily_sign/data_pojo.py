20766681dcfefe3a9e763c6419e8f84cccaab068
DuNai0524 <1094898402@qq.com>
2025-09-12 11:41:56 +0800

    重构代码

--- src/plugins/dunai_daily_sign/data_pojo.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class UserData(BaseModel):
-    all_gold: int
-    today_gold: int
-    sign_times: int
-    today_lucky: int
--- src/plugins/dunai_get_award/__init__.py
+++
@@ -1,3 +0,0 @@
-from nonebot_plugin_tortoise_orm import add_model
-
-add_model("src.plugins.dunai_get_award.models")
