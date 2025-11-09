2413dd10820d79afd9f5bb18cee97ae281beb8b7
DuNai0524 <1094898402@qq.com>
2025-11-09 23:47:44 +0800

    搁置 maim i插件开发

--- src/plugins/plugin_maimaidx/lib/api.py
+++
@@ -1,4 +0,0 @@
-class maimaiAPI:
-    PROBER_API = "https://www.diving-fish.com/api/maimaidxprober"
-    COVER_API = "https://www.diving-fish.com/covers/"
-    ALIAS_API = "https://api.yuzuchan.moe/maimaidx"
